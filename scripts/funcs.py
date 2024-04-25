a8eda96d230a519151727bc240471a4f157ea0e2
Isaiah Guenther <izguenther6@gmail.com>
2024-04-24 22:30:55 -0400

    perceptron
    - worked on data organization
    - lots of work on perceptron...accuracy testing

--- scripts/funcs.py
+++ scripts/funcs.py
@@ -6,7 +6,7 @@ import pandas as pd
 import numpy as np
 import numpy.random as npr
 
-def feat_eng_nom(df, colName):
+def nominal(df, colName):
     '''
     assigns values to nominal data
     ---
@@ -29,37 +29,9 @@ def feat_eng_nom(df, colName):
 
     return df, categories
 
-def onehot(df=None, labels=None, train_x=None):
-    '''
-    performs one-hot encoding for categories
-    ---
-    returns: cols, numpy array of the onehot encoded data
-    ---
-    '''
-    cols = np.empty((len(df),0))
+     
 
-    for label in labels:
-        onehot = pd.get_dummies(df[label])
-        train_cols = pd.get_dummies(train_x[label]).columns
-        #check if the onehot dataframe has the same columns as train...this only matters for the test values
-        #if not, need to create onehot encoding in different way
-        if onehot.columns.all() != train_cols.all():
-            #new onehot df that matches train_cols
-            onehot = pd.DataFrame(columns=train_cols, index = range(len(df)))
-            
-            #set all values to for each category, but then make correct value 1
-            for i in range(len(df)):
-               onehot.iloc[i,:] = 0
-               cat = df[label].iloc[i]
-
-               #make sure that category is in train_cols
-               if cat in train_cols:
-                onehot.at[i, cat] = 1
-
-        cols = np.concatenate((np.asarray(onehot),cols), axis = 1)
-    return cols
-
-def onehot_2(df=None, columns=None, train_x=None):
+def onehot(df=None, columns=None):
     '''
     onehot encoder
     ---
@@ -73,6 +45,9 @@ def onehot_2(df=None, columns=None, train_x=None):
 
     #encode through every col in columns
     for col in columns:
+        onehot = pd.get_dummies(df[col])
+        df_onehot = pd.concat([onehot, df_onehot], axis = 1)
+        '''
         train_cols = pd.get_dummies(train_x[col]).columns
         onehot = pd.DataFrame(0, columns=train_cols, index=range(len(df)))
         for i in range(len(df)):
@@ -84,10 +59,10 @@ def onehot_2(df=None, columns=None, train_x=None):
                 onehot.at[i, entry] = 1
 
         df_onehot = pd.concat([onehot, df_onehot], axis = 1)
-    
+        '''
     return df_onehot
 
-def perceptron(X=None,y=None,w_0=None,maxchecks=1000,maxiters=1000):
+def perceptron(X=None,y=None,w_0=None,maxchecks=10000,maxiters=10000):
     '''
     perceptron algorithm from homework 2
     ---
@@ -106,9 +81,7 @@ def perceptron(X=None,y=None,w_0=None,maxchecks=1000,maxiters=1000):
     step = 0
     laststep = 0
     for i in range(0,maxchecks):
-        print('iters: ' + str(step))
-        print('checks: ' + str(i))
-        if np.dot(y.iloc[i%n, 0] * X.iloc[i%n], w) < 0: # Loop over the data points until we find one that violates perceptron condition
+        if np.dot(y.iloc[i%n,0] * X.iloc[i%n], w) < 0: # Loop over the data points until we find one that violates perceptron condition
             step += 1
             laststep = i
             w += y.iloc[i%n,0] * X.iloc[i%n]
@@ -118,7 +91,19 @@ def perceptron(X=None,y=None,w_0=None,maxchecks=1000,maxiters=1000):
             #if plot_progress:
                 #plot_perceptron(X,y,w)
             break
-                
+     
     return w
 
-   
+def sign(val):
+    '''
+    get sign for perceptron loss evaluation
+    ---
+    returns: sign, the result from the sign function for dot(w,x)
+    ---
+    args: result of dot(w,x)
+    '''
+    sign = 1
+    if val < 0:
+        sign = -1
+
+    return sign
